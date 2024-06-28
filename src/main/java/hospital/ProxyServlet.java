b7466b67c7ef2d96d726ae089ad13e2a6c767d0a
joshHam <163796630+joshHam@users.noreply.github.com>
2024-06-28 09:35:14 +0900

    서블렛충돌fix

--- src/main/java/hospital/ProxyServlet.java
+++ src/main/java/hospital/ProxyServlet.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/ProxyServlet")
+//@WebServlet("/ProxyServlet")
 public class ProxyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
